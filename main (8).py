b853bf1422948e8f3a480a401e56170b9b660fcd
Hinduja25 <144442886+Hinduja25@users.noreply.github.com>
2023-10-04 20:45:22 +0530

    Add files via upload

---
+++ main (8).py
@@ -0,0 +1,15 @@
+
+def linearsearchproduct(productlist, targetproduct):
+  indicies = []
+  for index, product in enumerate (productlist) :
+    if product == targetproduct:
+       induces.append(index)
+    return indicies 
+products=["shoes","bool","toafer","shoes","sandal","shoes"]
+target="shoes"
+target="apple"
+result= linearsearchproduct(products, target)
+print(result)
+
+
+  
